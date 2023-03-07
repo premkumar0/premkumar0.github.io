bc82746b2f56bd2af2284498c759ac695c28f4b3
premkumar <premkumarnunna.ce@gmail.com>
2023-03-07 15:28:05 +0530

    updated code

--- public/js/index.js
+++ public/js/index.js
@@ -4,9 +4,14 @@ window.onload = function digital_fn() {
     // toggle button
     let toggle = document.querySelector("#nav .toggle-btn");
     let collapse = document.querySelector("#nav .collapse");
+    let nav = document.querySelector("#navbarSupportedContent");
 
     toggle.addEventListener('click', function (event) {
         collapse.classList.toggle('active')
+        // console.log(toggle)
+    });
+    nav.addEventListener('click', function (event) {
+        collapse.classList.remove('active')
         // console.log(toggle)
     });
 
